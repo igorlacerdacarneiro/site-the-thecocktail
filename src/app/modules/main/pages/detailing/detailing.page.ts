e6e288f94d698aaa1d16bef253241ca915f1dcdb
igor.carneiro_hartbgroup <igor.carneiro_hartbgroup@prestador.globo>
2021-01-09 18:20:59 -0300

    feat: create for screen detailing drink

---
+++ src/app/modules/main/pages/detailing/detailing.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-detailing',
+  templateUrl: './detailing.page.html',
+  styleUrls: ['./detailing.page.scss'],
+})
+export class DetailingPage implements OnInit, OnDestroy {
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {}
+}
