f5d506309219f8db6461da7e91b6357611cb7ba0
igor.carneiro_hartbgroup <igor.carneiro_hartbgroup@prestador.globo>
2021-01-09 18:21:14 -0300

    feat: create screen for list drinks after filter

---
+++ src/app/modules/main/pages/drinks/drinks.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-drinks',
+  templateUrl: './drinks.page.html',
+  styleUrls: ['./drinks.page.scss'],
+})
+export class DrinksPage implements OnInit, OnDestroy {
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {}
+}
