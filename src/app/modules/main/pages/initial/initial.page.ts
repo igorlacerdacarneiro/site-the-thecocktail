4a6fa3a254a2fc55306bcca705118f38a1fc7f28
igor.carneiro_hartbgroup <igor.carneiro_hartbgroup@prestador.globo>
2021-01-09 18:21:49 -0300

    feat: create screen initial for select filter

---
+++ src/app/modules/main/pages/initial/initial.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-initial',
+  templateUrl: './initial.page.html',
+  styleUrls: ['./initial.page.scss'],
+})
+export class InitalPage implements OnInit, OnDestroy {
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {}
+}
