7763ec1e9ec52cdf577b13726e9427bf7bdb1774
igor.carneiro_hartbgroup <igor.carneiro_hartbgroup@prestador.globo>
2021-01-10 21:58:55 -0300

    refactor: change general in screen initial in css, logic and actions

--- src/app/modules/main/pages/initial/initial.page.ts
+++ src/app/modules/main/pages/initial/initial.page.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { InitialFacadeService } from '../../facades/initial-facade.service';
 
 @Component({
   selector: 'app-initial',
   templateUrl: './initial.page.html',
   styleUrls: ['./initial.page.scss'],
 })
-export class InitalPage implements OnInit, OnDestroy {
+export class InitalPage implements OnInit {
 
-  constructor() {}
+  select: string;
 
-  ngOnInit(): void {}
+  constructor(private router: Router, public initalFacade: InitialFacadeService) { }
 
-  ngOnDestroy(): void {}
+  ngOnInit(): void { }
+
+  selectFilter(type: string) {
+    this.select = type;
+    this.initalFacade.selectType(type);
+    switch (type) {
+      case 'c':
+        this.initalFacade.loadCategories({ c: 'list' }).toPromise();
+        break;
+      case 'g':
+        this.initalFacade.loadGlasses({ g: 'list' }).toPromise();
+        break;
+      case 'i':
+        this.initalFacade.loadIngredients({ i: 'list' }).toPromise();
+        break;
+      default:
+        this.initalFacade.loadAlcoholics({ a: 'list' }).toPromise();
+        break;
+    }
+  }
+
+  selectedFilter(item: string) {
+    let newItem = item.replace(/\//g, '');
+    newItem = newItem.replace(/\s/g, '_');
+    switch (this.select) {
+      case 'c':
+        this.router.navigate([`drinks/c-${newItem}`]);
+        break;
+      case 'g':
+        this.router.navigate([`drinks/g-${newItem}`]);
+        break;
+      case 'i':
+        this.router.navigate([`drinks/i-${newItem}`]);
+        break;
+      default:
+        this.router.navigate([`drinks/a-${newItem}`]);
+        break;
+    }
+  }
+
+  categoriessTrackFn = (i, item) => item.strCategory;
+
+  glassesTrackFn = (i, item) => item.strGlass;
+
+  ingredientsTrackFn = (i, item) => item.strIngredient1;
+
+  alcoholicsTrackFn = (i, item) => item.strAlcoholic;
 }
