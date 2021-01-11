0186158ecd464b7bc8c683a766065e9bcd33f95c
igor.carneiro_hartbgroup <igor.carneiro_hartbgroup@prestador.globo>
2021-01-10 21:51:21 -0300

    feat: create page detail drink selected

--- src/app/modules/main/pages/detailing/detailing.page.ts
+++ src/app/modules/main/pages/detailing/detailing.page.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewContainerRef } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ActionType } from 'src/app/shared/enums/action-types';
+import { IButton } from 'src/app/shared/models/button.interface';
+import { IHeaderOptions } from 'src/app/shared/models/header.interface';
+import { HeaderService } from 'src/app/shared/services/header.service';
+import { ModalEvaluateComponent } from '../../components/modal-evaluate/modal-evaluate.component';
+import { ModalSharedComponent } from '../../components/modal-shared/modal-shared.component';
+import { DetailingFacadeService } from '../../facades/detailing-facade.service';
 
 @Component({
   selector: 'app-detailing',
@@ -7,9 +15,88 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class DetailingPage implements OnInit, OnDestroy {
 
-  constructor() {}
+  subscriptions: Subscription[] = [];
 
-  ngOnInit(): void {}
+  constructor(
+    public detailingFacade: DetailingFacadeService,
+    private headerService: HeaderService,
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private vcr: ViewContainerRef,
+  ) {
+    this.subscriptions.push(
+      this.headerService.buttonTrigger.subscribe((button: IButton) => {
+        if (button.action === ActionType.SEND) {
+          this.openModalSend();
+        }
 
-  ngOnDestroy(): void {}
+        if (button.action === ActionType.UPDATE) {
+          this.openModalUpdate();
+        }
+      })
+    )
+  }
+
+  ngOnInit(): void { }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  }
+
+  get headerOptions(): IHeaderOptions {
+    return {
+      title: `Drink - ${this.detailingFacade.drink.strDrink}`,
+      titleColor: 'black',
+      hideBackButton: false,
+      buttons: this.getButtons()
+    };
+  }
+
+  private getButtons(): IButton[] {
+    const buttons = [];
+
+    buttons.push({
+      label: 'Compartilhar',
+      action: ActionType.SEND,
+    });
+
+    buttons.push({
+      label: 'Avaliar',
+      action: ActionType.UPDATE,
+    });
+
+    return buttons;
+  }
+
+  openModalSend() {
+    this.vcr.clear();
+
+    const factory = this.componentFactoryResolver.resolveComponentFactory(
+      ModalSharedComponent,
+    );
+
+    const ref = this.vcr.createComponent(factory);
+
+    ref.instance.drink = this.detailingFacade.drink;
+
+    ref.changeDetectorRef.detectChanges();
+
+    ref.instance.closeEventEmitter.subscribe(() => ref.destroy());
+  }
+
+  openModalUpdate() {
+    this.vcr.clear();
+
+    const factory = this.componentFactoryResolver.resolveComponentFactory(
+      ModalEvaluateComponent,
+    );
+
+    const ref = this.vcr.createComponent(factory);
+    
+    ref.instance.drink = this.detailingFacade.drink;
+
+    ref.changeDetectorRef.detectChanges();
+
+    ref.instance.closeEventEmitter.subscribe(() => ref.destroy());
+  }
+
 }
