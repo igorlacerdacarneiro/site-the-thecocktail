8bd4e68e7b8c4c2dace67d31b51886ea50493dd0
igor.carneiro_hartbgroup <igor.carneiro_hartbgroup@prestador.globo>
2021-01-10 21:48:22 -0300

    refactor: main route for controle general app

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -4,10 +4,31 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { MainPage } from './main.page';
+import { SideFilterComponent } from './components/side-filter/side-filter.component';
+import { InitalPage } from './pages/initial/initial.page';
+import { DrinksPage } from './pages/drinks/drinks.page';
+import { DetailingPage } from './pages/detailing/detailing.page';
+import { CardDrinkComponent } from './components/card-drink/card-drink.component';
+import { ModalSharedComponent } from './components/modal-shared/modal-shared.component';
+import { ModalEvaluateComponent } from './components/modal-evaluate/modal-evaluate.component';
 
 @NgModule({
-  declarations: [MainPage],
+  declarations: [
+    MainPage,
+    InitalPage,
+    DrinksPage,
+    DetailingPage,
+    SideFilterComponent,
+    CardDrinkComponent,
+    ModalSharedComponent,
+    ModalEvaluateComponent
+  ],
   imports: [CommonModule, MainRoutingModule, SharedModule],
-  exports: [MainPage],
+  exports: [MainPage,
+    SideFilterComponent,
+    CardDrinkComponent,
+    ModalSharedComponent,
+    ModalEvaluateComponent
+  ],
 })
-export class MainModule {}
+export class MainModule { }
--- src/app/modules/main/main.page.ts
+++ src/app/modules/main/main.page.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.page.scss'],
 })
 export class MainPage implements OnInit {
-  ngOnInit(): void {
-    console.log('MainPage');
-  }
+
+  ngOnInit(): void {}
 }
