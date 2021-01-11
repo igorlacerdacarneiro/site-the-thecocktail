c560da1cfb8fcfb505716a719a87504029fb7298
igor.carneiro_hartbgroup <igor.carneiro_hartbgroup@prestador.globo>
2021-01-10 21:58:26 -0300

    refactor: add in module shared component create and libs

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveFormsModule } from '@angular/forms';
-import { SearchComponent } from './components/search/search.component';
 import { PaginationComponent } from './components/pagination/pagination.component';
 import { TopMenuComponent } from './components/top-menu/top-menu.component';
 import { LogoutComponent } from './components/logout/logout.component';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HeaderButtonsComponent } from './components/header-buttons/header-buttons.component';
 
 @NgModule({
-  declarations: [SearchComponent, PaginationComponent, TopMenuComponent, LogoutComponent],
+  declarations: [
+    PaginationComponent,
+    TopMenuComponent,
+    LogoutComponent,
+    HeaderComponent,
+    HeaderButtonsComponent,
+  ],
   imports: [CommonModule, ReactiveFormsModule, RouterModule],
-  exports: [ReactiveFormsModule, SearchComponent, PaginationComponent, TopMenuComponent, LogoutComponent],
+  exports: [
+    ReactiveFormsModule,
+    PaginationComponent,
+    TopMenuComponent,
+    LogoutComponent,
+    HeaderComponent,
+    HeaderButtonsComponent,
+  ],
 })
 export class SharedModule { }
